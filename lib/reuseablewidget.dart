8c8a131b24ee072f8ab9d38e32fb2ef34a631c50
unknown <sohraoulsuffo@gmail.com>
2022-09-24 18:30:43 +0100

    sign up page completed remaining only the logo

--- lib/reuseablewidget.dart
+++ lib/reuseablewidget.dart
@@ -12,7 +12,7 @@ TextField reusableTextField(String text, IconData icon, bool ispassword,
     decoration: InputDecoration(
         prefixIcon: Icon(icon, color: Colors.black),
         filled: true,
-        fillColor: Colors.grey.withOpacity(0.1),
+        fillColor: Colors.grey.withOpacity(0.3),
         border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(35.0),
             borderSide: BorderSide(width: 0, style: BorderStyle.none))),
@@ -28,5 +28,34 @@ Image logoWidget(String logoname) {
     width: 100,
     height: 100,
     color: Colors.orange,
+  );
+}
+
+Container signupButton(String btnName, BuildContext context, Function Ontap) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    height: 50,
+    margin: EdgeInsets.fromLTRB(0, 30, 0, 10),
+    decoration: BoxDecoration(borderRadius: BorderRadius.circular(50)),
+    child: ElevatedButton(
+      onPressed: () {
+        Ontap();
+      },
+      child: Text(
+        btnName,
+        style: TextStyle(
+            color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+      ),
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.resolveWith((states) {
+            if (states.contains(MaterialState.pressed)) {
+              return Colors.black;
+            } else {
+              return Colors.orange;
+            }
+          }),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)))),
+    ),
   );
 }
--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -11,7 +11,7 @@ class loginPage extends StatefulWidget {
 
 class _loginPageState extends State<loginPage> {
   TextEditingController controller = TextEditingController();
-
+  bool remember_me = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,30 +42,60 @@ class _loginPageState extends State<loginPage> {
                     false,
                     controller,
                   ),
+                  SizedBox(height: 10.0),
                   reusableTextField(
                     'Last name',
                     Icons.person_outline,
                     false,
                     controller,
                   ),
+                  SizedBox(height: 10.0),
                   reusableTextField(
                     'Email Adress',
-                    Icons.person_outline,
+                    Icons.email_outlined,
                     false,
                     controller,
                   ),
+                  SizedBox(height: 10.0),
                   reusableTextField(
                     'password',
-                    Icons.person_outline,
+                    Icons.lock_outline,
                     true,
                     controller,
                   ),
+                  SizedBox(height: 10.0),
                   reusableTextField(
                     'Confirm password',
-                    Icons.person_outline,
+                    Icons.lock_outline,
                     true,
                     controller,
-                  )
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Transform.scale(
+                          scale: 1.3,
+                          child: Checkbox(
+                            checkColor: Colors.white,
+                            activeColor: Colors.orange,
+                            side: BorderSide(color: Colors.orange, width: 2.0),
+                            value: remember_me,
+                            onChanged: (bool? value) {
+                              setState(() {
+                                remember_me = value!;
+                              });
+                            },
+                          ),
+                        ),
+                        Text(
+                          'Remember me',
+                          style: TextStyle(fontSize: 23),
+                        )
+                      ],
+                    ),
+                  ),
+                  signupButton('Sign Up', context, () {})
                 ],
               ))
         ],
