122f3692f6bc22bf74bb0293801db9f427c10ec2
unknown <sohraoulsuffo@gmail.com>
2022-09-27 20:54:29 +0100

    firebase authentiaction completed

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:login/screens/homescreen.dart';
 import 'package:login/screens/signuppage.dart';
@@ -19,89 +20,97 @@ class _loginPageState extends State<loginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-              //logo
-              ),
-          Container(
-            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 30, 10, 15),
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                //logo
+                ),
+            Container(
+              margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 30, 10, 15),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 10.0),
-                reusableTextField('Email Adress', Icons.email_outlined, false,
-                    _emailcontroller, 'Email Adress'),
-                SizedBox(height: 10.0),
-                reusableTextField('password', Icons.lock_outline, true,
-                    _passwordcontroller, 'password'),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Transform.scale(
-                        scale: 1.3,
-                        child: Checkbox(
-                          checkColor: Colors.white,
-                          activeColor: Colors.orange,
-                          side: BorderSide(color: Colors.orange, width: 2.0),
-                          value: remember_me,
-                          onChanged: (bool? value) {
-                            setState(() {
-                              remember_me = value!;
-                            });
-                          },
+                  SizedBox(height: 10.0),
+                  reusableTextField('Email Adress', Icons.email_outlined, false,
+                      _emailcontroller, 'Email Adress'),
+                  SizedBox(height: 10.0),
+                  reusableTextField('password', Icons.lock_outline, true,
+                      _passwordcontroller, 'password'),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Transform.scale(
+                          scale: 1.3,
+                          child: Checkbox(
+                            checkColor: Colors.white,
+                            activeColor: Colors.orange,
+                            side: BorderSide(color: Colors.orange, width: 2.0),
+                            value: remember_me,
+                            onChanged: (bool? value) {
+                              setState(() {
+                                remember_me = value!;
+                              });
+                            },
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Remember me',
-                        style: TextStyle(fontSize: 23),
-                      )
-                    ],
+                        Text(
+                          'Remember me',
+                          style: TextStyle(fontSize: 23),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                signupButton('Login', context, () {
-                  Navigator.push((context),
-                      MaterialPageRoute(builder: (context) {
-                    return HomeScreen();
-                  }));
-                })
-              ],
+                  signupButton('Login', context, () {
+                    FirebaseAuth.instance
+                        .signInWithEmailAndPassword(
+                            email: _emailcontroller.text,
+                            password: _passwordcontroller.text)
+                        .then((value) {
+                      Navigator.push((context),
+                          MaterialPageRoute(builder: (context) {
+                        return HomeScreen();
+                      }));
+                    });
+                  })
+                ],
+              ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
-            child: Row(
-              children: [
-                Text('Do you have an acount?',
-                    style: TextStyle(color: Colors.black, fontSize: 15.0)),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push((context),
-                        MaterialPageRoute(builder: (context) {
-                      return SignUpPage();
-                    }));
-                  },
-                  child: Text(
-                    'Sign up',
-                    style: TextStyle(color: Colors.black, fontSize: 18.0),
+            Container(
+              margin: EdgeInsets.fromLTRB(20, 10, 20, 0),
+              child: Row(
+                children: [
+                  Text('Do you have an acount?',
+                      style: TextStyle(color: Colors.black, fontSize: 15.0)),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push((context),
+                          MaterialPageRoute(builder: (context) {
+                        return SignUpPage();
+                      }));
+                    },
+                    child: Text(
+                      'Sign up',
+                      style: TextStyle(color: Colors.black, fontSize: 18.0),
+                    ),
                   ),
-                ),
-              ],
-            ),
-          )
-        ],
+                ],
+              ),
+            )
+          ],
+        ),
       )),
     );
   }
--- lib/screens/signuppage.dart
+++ lib/screens/signuppage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:login/screens/loginpage.dart';
 import '../reuseablewidget.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
@@ -20,91 +22,103 @@ class _SignUpPageState extends State<SignUpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          child: Column(
-        children: [
-          Container(
-              margin: EdgeInsets.only(top: 30),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+                margin: EdgeInsets.only(top: 30),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Dairy',
+                        style: TextStyle(fontSize: 50.0, color: Colors.orange),
+                      ),
+                      Text(
+                        'Market',
+                        style: TextStyle(
+                            fontSize: 40.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ])),
+            Container(
+                margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text(
-                      'Dairy',
-                      style: TextStyle(fontSize: 50.0, color: Colors.orange),
-                    ),
-                    Text(
-                      'Market',
-                      style: TextStyle(
-                          fontSize: 40.0,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold),
-                    )
-                  ])),
-          Container(
-              margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    margin: EdgeInsets.fromLTRB(10, 30, 10, 15),
-                    child: Text(
-                      'Create Acount',
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
+                    Container(
+                      margin: EdgeInsets.fromLTRB(10, 30, 10, 15),
+                      child: Text(
+                        'Create Acount',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                  reusableTextField('First name', Icons.person_outline, false,
-                      _firstName_controller, 'First name'),
-                  SizedBox(height: 10.0),
-                  reusableTextField('Last name', Icons.person_outline, false,
-                      _lastName_controller, 'Last name'),
-                  SizedBox(height: 10.0),
-                  reusableTextField('Email Adress', Icons.email_outlined, false,
-                      _email_controller, 'Email Adress'),
-                  SizedBox(height: 10.0),
-                  reusableTextField('password', Icons.lock_outline, true,
-                      _password_controller, 'password'),
-                  SizedBox(height: 10.0),
-                  reusableTextField('Confirm password', Icons.lock_outline,
-                      true, _confirmPassword_controller, 'Confirm password'),
-                  Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Transform.scale(
-                          scale: 1.3,
-                          child: Checkbox(
-                            checkColor: Colors.white,
-                            activeColor: Colors.orange,
-                            side: BorderSide(color: Colors.orange, width: 2.0),
-                            value: remember_me,
-                            onChanged: (bool? value) {
-                              setState(() {
-                                remember_me = value!;
-                              });
-                            },
+                    reusableTextField('First name', Icons.person_outline, false,
+                        _firstName_controller, 'First name'),
+                    SizedBox(height: 10.0),
+                    reusableTextField('Last name', Icons.person_outline, false,
+                        _lastName_controller, 'Last name'),
+                    SizedBox(height: 10.0),
+                    reusableTextField('Email Adress', Icons.email_outlined,
+                        false, _email_controller, 'Email Adress'),
+                    SizedBox(height: 10.0),
+                    reusableTextField('password', Icons.lock_outline, true,
+                        _password_controller, 'password'),
+                    SizedBox(height: 10.0),
+                    reusableTextField('Confirm password', Icons.lock_outline,
+                        true, _confirmPassword_controller, 'Confirm password'),
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Transform.scale(
+                            scale: 1.3,
+                            child: Checkbox(
+                              checkColor: Colors.white,
+                              activeColor: Colors.orange,
+                              side:
+                                  BorderSide(color: Colors.orange, width: 2.0),
+                              value: remember_me,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  remember_me = value!;
+                                });
+                              },
+                            ),
                           ),
-                        ),
-                        Text(
-                          'Remember me',
-                          style: TextStyle(fontSize: 23),
-                        )
-                      ],
+                          Text(
+                            'Remember me',
+                            style: TextStyle(fontSize: 23),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  signupButton('Sign Up', context, () {
-                    Navigator.push(
-                      (context),
-                      MaterialPageRoute(builder: (context) {
-                        return loginPage();
-                      }),
-                    );
-                  }),
-                ],
-              ))
-        ],
+                    signupButton('Sign Up', context, () {
+                      FirebaseAuth.instance
+                          .createUserWithEmailAndPassword(
+                              email: _email_controller.text,
+                              password: _password_controller.text)
+                          .then((value) {
+                        Navigator.push(
+                          (context),
+                          MaterialPageRoute(builder: (context) {
+                            return loginPage();
+                          }),
+                        );
+                      }).onError((error, stackTrace) {
+                        print('errrroooor!!');
+                        print(error);
+                      });
+                    }),
+                  ],
+                ))
+          ],
+        ),
       )),
     );
   }
